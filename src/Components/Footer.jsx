f925b060179b056efb31dc4a8d8b239ca22424ab
Toki-63 <asimtoki516@gmail.com>
2026-05-22 15:29:02 +0600

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,13 +12,13 @@ import { VscSend } from "react-icons/vsc";
 const Footer = () => {
   return (
     <>
-      <footer className='bg-black  mt-[140px]'>
+      <footer className='bg-black  lg:mt-35 mt-15'>
         <Container>
-          <Flex className='justify-between py-20'>
+          <div className='lg:flex justify-between lg:py-20 py-10 lg:mx-0 mx-auto lg:text-start text-center'>
             <div>
               <div>
                 <h1 className='text-white font-bold text-2xl font-Inter'>Exclusive</h1>
-                <h2 className='text-xl font-medium font-poppins py-6 text-white '>Subscribe</h2>
+                <h2 className='text-xl font-medium font-poppins lg:py-6 py-2 text-white '>Subscribe</h2>
                 <p className='font-poppins pb-4 text-white'>Get 10% off your first order</p>
                 <input
                   type='text'
@@ -26,18 +26,18 @@ const Footer = () => {
                   placeholder="Enter your email"
                 />
                 <div className='relative'>
-                  <VscSend className='absolute lg:right-14 right-6 -top-6.5 text-white cursor-pointer' />
+                  <VscSend className='absolute lg:right-14 right-24 -top-6.5 text-white cursor-pointer' />
                 </div>
               </div>
             </div>
-            <div className='font-poppins w-43.75 lg:mx-0 text-start text-white '>
+            <div className='font-poppins w-43.75 lg:mx-0 mx-auto mt-10 lg:mt-0 lg:text-start text-center text-white '>
               <h2 className='text-xl font-medium'>Support</h2>
               <p className='lg:pt-6 lg:pb-4 pt-2 pb-1'>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</p>
               <h2 className='lg:pb-4 pb-1'>exclusive@gamil.com</h2>
               <h2>+88015-88888-9999</h2>
             </div>
 
-            <ul className="lg:mx-0 mx-auto lg:text-start text-center text-white">
+            <ul className="lg:mx-0 mx-auto lg:text-start text-center text-white mt-10 lg:mt-0">
               <h2 className='text-xl font-medium font-poppins'>Account</h2>
               <li className='lg:pt-6 lg:pb-4 pt-3 pb-1'>My Account</li>
               <li>Login / Register</li>
@@ -46,7 +46,7 @@ const Footer = () => {
               <li>Shop</li>
             </ul>
 
-            <ul className="lg:mx-0 mx-auto lg:text-start text-center text-white">
+            <ul className="lg:mx-0 mx-auto lg:text-start mt-10 lg:mt-0 text-center text-white">
               <h2 className='text-xl font-medium font-poppins'>Quick Link</h2>
               <li className='lg:pt-6 lg:pb-4 pt-3 pb-1'>Privacy Policy</li>
               <li>Terms Of Use</li>
@@ -54,19 +54,19 @@ const Footer = () => {
               <li>Contact</li>
             </ul>
 
-            <div className='lg:mx-0 mx-auto lg:text-start text-center text-white'>
+            <div className='lg:mx-0 mx-auto lg:text-start mt-10 lg:mt-0 text-center text-white'>
               <h2 className='text-xl font-medium font-poppins'>Download App</h2>
-              <p className='text-xs font-poppins font-medium lg:pt-6 lg:pb-4 pt-3 pb-1'>Save $3 with App New User Only</p>
-              <img src={QR} alt="" />
-              <div className='flex justify-around gap-2 pt-6 text-2xl'>
+              <p className='text-xs font-poppins lg:font-medium font-bold lg:pt-6 lg:pb-4 pt-3 pb-1'>Save $3 with App New User Only</p>
+              <img src={QR} alt="" className='mx-auto lg:mx-0' />
+              <div className='flex lg:justify-around  gap-6 mx-auto ml-30 text-2xl lg:mt-0 mt-4'>
                 <FaFacebookF />
                 <CiTwitter />
                 <FaInstagram />
                 <FaLinkedinIn />
               </div>
             </div>
-          </Flex>
-          <div className='flex justify-center items-center text-[#353434b3] gap-1 mt-[76px]'>
+          </div>
+          <div className='flex justify-center items-center text-[#353434b3] gap-1 lg:mt-19'>
             <FaRegCopyright />
             <h2 className='text-[#353434b3] text-center py-4'>Copyright Rimel 2022. All right reserved</h2>
           </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { NavLink} from "react-router";
 
 const Header = () => {
   return (
     <div>
-      <header className='bg-black py-3 text-white'>
+      <header className='bg-black py-3 px-2 lg:px-0 text-white'>
         <div className="container">
          <div className='flex justify-between'>
-            <div className='w-[70%] flex justify-end'>
-              <p className='flex gap-2 text-sm'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-              <a href="">ShopNow</a></p>
+            <div className='lg:w-[70%] w-full text-center flex justify-end'>
+              <p className='lg:flex gap-2 lg:text-sm text-[10px] font-Poppins '>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+                <NavLink to="/shop" end className='font-bold underline ml-1'>
+                    Shop Now
+                  </NavLink>
+              </p>
             </div>
             <div className='w-[30%] flex justify-end'>
           <button className='flex items-center gap-1.25 text-sm'>English <MdOutlineKeyboardArrowDown /></button>
